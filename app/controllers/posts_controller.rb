644f263a60c8d88f56e688049f8358c50eceff26
mikuto17 <ishi17hara@gmail.com>
2023-10-27 16:14:04 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  def index
+  end
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to "/posts"
+  end
+end
